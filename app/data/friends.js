5c6441d639774f2ff36a1b5bddd77a489fe3c931
Corey Richard <ullrtheglorious@gmail.com>
2018-04-23 14:19:06 -0700

    html working along with basic functions

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friendsArray = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            5,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+
+    },
+    {
+        name: "johanna",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            5,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+
+    },
+    {
+        name: "john",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            5,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+
+    }
+];
+module.exports = friendsArray;
