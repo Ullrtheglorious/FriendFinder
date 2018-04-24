c89e6754eb50bc8fc86b41534cef5a5687b1aded
Corey Richard <ullrtheglorious@gmail.com>
2018-04-24 12:11:15 -0700

    profile changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
-        name: "Ahmed",
-        photo: "https://s3.amazonaws.com/com-aab-media/photo/59858041-720px.jpg",
+        name: "harold",
+        photo: "https://i.imgur.com/Vofq5Cp.jpg",
         scores: [
             5,
             5,
@@ -18,15 +18,66 @@ var friendsArray = [
     },
     {
         name: "johanna",
-        photo: "https://s3.amazonaws.com/com-aab-media/photo/59858041-720px.jpg",
+        photo: "https://thumb9.shutterstock.com/display_pic_with_logo/580987/766032385/stock-photo-close-up-portrait-of-a-hungry-greedy-girl-eating-donuts-isolated-over-white-background-766032385.jpg",
+        scores: [
+            3,
+            3,
+            4,
+            4,
+            3,
+            1,
+            2,
+            3,
+            2,
+            1
+        ]
+
+    },
+    {
+        name: "vanessa",
+        photo: "https://thumb9.shutterstock.com/display_pic_with_logo/2936380/696230005/stock-photo-horizontal-portrait-of-pleasant-looking-caucasian-female-with-long-hair-pink-on-tips-having-696230005.jpg",
         scores: [
             5,
             5,
+            1,
             4,
-            4,
-            5,
+            2,
             1,
             2,
+            5,
+            1,
+            1
+        ]
+
+    },
+    {
+        name: "doug",
+        photo: "https://thumb9.shutterstock.com/display_pic_with_logo/2936380/776211322/stock-photo-unshaven-confident-serious-young-male-enterpreneur-with-attractive-appearance-dressed-casually-as-776211322.jpg",
+        scores: [
+            1,
+            1,
+            4,
+            1,
+            2,
+            4,
+            2,
+            5,
+            2,
+            4
+        ]
+
+    },
+    {
+        name: "nick",
+        photo: "https://image.shutterstock.com/z/stock-photo-cheerful-hipster-guy-smiles-happily-has-excited-expression-dresssed-casually-celebrates-his-744198382.jpg",
+        scores: [
+            5,
+            4,
+            4,
+            4,
+            2,
+            1,
+            4,
             5,
             4,
             1
@@ -34,18 +85,86 @@ var friendsArray = [
 
     },
     {
-        name: "john",
-        photo: "https://s3.amazonaws.com/com-aab-media/photo/59858041-720px.jpg",
+        name: "jess",
+        photo: "https://thumb1.shutterstock.com/display_pic_with_logo/2936380/613759379/stock-photo-happy-cheerful-young-woman-wearing-her-red-hair-in-bun-rejoicing-at-positive-news-or-birthday-gift-613759379.jpg",
+        scores: [
+            5,
+            2,
+            4,
+            3,
+            3,
+            1,
+            2,
+            5,
+            2,
+            1
+        ]
+
+    },
+    {
+        name: "alex",
+        photo: "https://thumb1.shutterstock.com/display_pic_with_logo/2561866/384352516/stock-photo-handsome-caucasian-guy-takes-a-selfie-at-the-beach-people-lifestyle-and-technology-concept-384352516.jpg",
+        scores: [
+            3,
+            5,
+            3,
+            4,
+            3,
+            3,
+            2,
+            3,
+            3,
+            1
+        ]
+
+    },
+    {
+        name: "casey",
+        photo: "https://thumb9.shutterstock.com/display_pic_with_logo/2967241/666258808/stock-photo-portrait-of-young-beautiful-cute-cheerful-girl-smiling-looking-at-camera-over-white-background-666258808.jpg",
         scores: [
             5,
             5,
             4,
-            4,
-            2,
+            5,
+            5,
             1,
             2,
             5,
-            9,
+            5,
+            4
+        ]
+
+    },
+    {
+        name: "sam",
+        photo: "https://image.shutterstock.com/z/stock-photo-positive-excited-handsome-guy-in-suit-hat-black-sunglasses-having-fun-on-white-background-577649716.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            2,
+            2,
+            1,
+            3,
+            3,
+            3,
+            1
+        ]
+
+    },
+    {
+        name: "patty",
+        photo: "https://thumb7.shutterstock.com/display_pic_with_logo/2967241/623804987/stock-photo-headshot-portrait-of-happy-ginger-girl-with-freckles-smiling-looking-at-camera-white-background-623804987.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1
         ]
 
